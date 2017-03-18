7cbf66ea0d5329b432010bc8320543c48b0a70b6
Struan <struanmcd@hotmaill.com>
2017-03-18 12:31:32 +0000

    Alexa will now connect to the first message of the newsletter

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ It's of a very consistent format, and I'm a hater of emails, so this works.
 * *"Is there anything happening at the university today?"*
 * *"Tell me what is happening this week"*
 * *"Is there anything happening on Wednesday?*
+
+## Licence
+
+This project is covered by the MIT licence. In other words, you may do with it what you want, as long as you don't blame
+me for anything which breaks on your computer.
--- index.js
+++ index.js
@@ -1,15 +1,73 @@
 // Alexa is the object which we're given, so we can interact with the Alexa API
 var Alexa = require('alexa-sdk');
 var WebsiteData = require('./util/scrape');
+var APP_ID = require('./exclude/id'); // Replace with your own application ID
+
+var mailMessages;
+var alreadyRetrieved = false;
+
+var languageStrings = {
+    "en": {
+        "translation": {
+            "SKILL_NAME": "The Monday Mail",
+            "WELCOME_MESSAGE": "Welcome to Kilburn's Monday Mail. If you need help, just ask!",
+            "HELP_MESSAGE": "You can ask me what's new. While a message is playing, you can also ask to skip the message, to go back, or to repeat the message, just like an answer phone! What can I help you with?",
+            "HELP_REPROMPT": "What can I help you with?",
+            "STOP_MESSAGE": "Goodbye!",
+            "UNHANDLED_MESSAGE": "Sorry, I didn't understand your question. How can I help?"
+        }
+    },
+    "de": {
+        "translation": {
+            "TODO": "Do we have any Germans in Kilburn?"
+        }
+    }
+};
 
 // Alexa is based around an event system, anything we code is a reaction to a series of events, so we need
 // to register everything we code here, so we're able to react to how the user speaks to the system
 exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
 
-WebsiteData.then(function(result){
-    console.log(result);
-});
-
-
+var handlers = {
+    'LaunchRequest': function(){
+        this.emit('Welcome');
+    },
+    'Welcome': function(){
+        var speechOutput = this.t("WELCOME_MESSAGE");
+        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), speechOutput);
+    },
+    'OverviewIntent': function(){
+        var thisAlexa = this;
+        if(!alreadyRetrieved){
+            WebsiteData.then(function (result) {
+                alreadyRetrieved = true;
+                mailMessages = result;
+                var speechOutput = mailMessages[0];
+                thisAlexa.emit(':tellWithCard', speechOutput, thisAlexa.t("SKILL_NAME"), speechOutput);
+            });
+        } else {
+            var speechOutput = mailMessages[0];
+            thisAlexa.emit(':tellWithCard', speechOutput, thisAlexa.t("SKILL_NAME"), speechOutput);
+        }
+    },
+    'AMAZON.HelpIntent': function(){
+        var speechOutput = this.t("HELP_MESSAGE");
+        var reprompt = this.t("HELP_MESSAGE");
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function(){
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function(){
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'Unhandled': function(){
+        this.emit(':ask', this.t("UNHANDLED_MESSAGE"));
+    }
+};
